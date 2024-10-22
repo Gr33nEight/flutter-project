02e701ce2fa246973a9dde4c2e4e9ff7e4a1d38e
Natanael Jop <natanael.jop@icloud.com>
2024-10-22 18:31:21 +0200

    Finished ui of login view

---
+++ lib/components/custom_textfield.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_n8/utils/my_colors.dart';
+
+class CustomTextfield extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool? obscureText;
+  final TextInputType keyboardType; 
+  final VoidCallback? onPressed;
+  final TextEditingController controller;
+
+  const CustomTextfield({
+    super.key, 
+    required this.text, 
+    required this.icon, 
+    this.obscureText,
+    this.keyboardType = TextInputType.text, 
+    this.onPressed,
+    required this.controller 
+  }) ;
+
+  @override
+  Widget build(BuildContext context) {
+
+    bool showPassword = !(obscureText != null ? obscureText! : false);
+
+    return TextFormField(
+      controller: controller,
+      obscureText: !showPassword && obscureText != null,
+      decoration: InputDecoration(
+        hintText: text,
+        hintStyle: const TextStyle(
+          color: Colors.grey
+        ),
+        prefixIcon: Icon(icon),
+        prefixIconColor: MyColors.purpleColor,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: MyColors.pinkColor,
+            width: 2.0
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: MyColors.pinkColor,
+            width: 2.0
+          ),
+          borderRadius: BorderRadius.circular(15),
+      ),
+      suffixIcon: IconButton(
+        icon: Icon(
+          showPassword ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+          size: obscureText != null ? 20 : 0),
+        onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/login/login_view.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_n8/components/custom_textfield.dart';
+import 'package:flutter_project_n8/utils/my_colors.dart';
+import 'package:flutter_project_n8/utils/my_images.dart';
+import 'package:flutter_project_n8/views/register/register_view.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool obscureText = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.whiteColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const Spacer(),
+            Image.asset(MyImages.logo, width: 129, height: 129),
+            const Spacer(),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Sign in',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700, color: MyColors.purpleColor),
+              ),
+            ),
+            const Spacer(),
+            CustomTextfield(
+              text: "Email or User Name",
+              icon: Icons.person_outline,
+              controller: _emailController
+            ),
+            const Spacer(),
+            CustomTextfield(
+              text: "Password",
+              icon: Icons.lock_outline,
+              controller: _passwordController,
+              obscureText: obscureText,
+              onPressed: () => {
+                setState(() {
+                  obscureText = !obscureText;
+                })
+              },
+            ),
+            const Spacer(),
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                child: Text(
+                  "Forget Password?",
+                  style: TextStyle(color: MyColors.purpleColor,  fontSize: 15, fontWeight: FontWeight.w700),
+                ),
+                onTap: () {},
+              ),
+            ),
+            const Spacer(),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  backgroundColor: MyColors.pinkColor
+                ),
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(color: MyColors.whiteColor, fontSize: 15, fontWeight: FontWeight.w700),
+                ),
+              )
+            ),
+            ),
+            const Spacer(),
+            GestureDetector(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have account? ",
+                    style: TextStyle(color: MyColors.purpleColor,  fontSize: 15, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    "Sign Up",
+                    style: TextStyle(color: MyColors.purpleColor,  fontSize: 15, fontWeight: FontWeight.w700),
+                  )
+                ],
+              ),
+              onTap: () {},
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
